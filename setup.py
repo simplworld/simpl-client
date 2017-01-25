6f67c3eb97dfa2ebc956fc05581d93947b7171ac
Flavio Curella <flavio.curella@gmail.com>
2017-01-25 15:57:30 -0600

    fix genericcclient version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Framework :: Django',
     ],
     install_requires=[
-        "genericclient==0.1.13",
+        "genericclient==0.0.13",
     ],
     test_suite='nose.collector',
     tests_require=[
